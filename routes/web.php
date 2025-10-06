6131bd84baa8af3259457a096b96f127d38b845f
Rkern91 <156999@upf.br>
2025-10-05 23:32:44 -0300

    Ajustado para mostrar tela de login como padrao

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,9 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
+  
 Route::get('/', function () {
-    return view('welcome');
+  return redirect()->route('login');
 });
 
 Route::get('/dashboard', function () {
